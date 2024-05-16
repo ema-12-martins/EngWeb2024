99b0ec15f69097902e2248f42be4d1244fd986b6
ema-12-martins <a97678@alunos.uminho.pt>
2024-05-16 15:53:42 +0100

    tudo

--- ENGWEB2024-Normal/ex1/controllers/contratos.js
+++ ENGWEB2024-Normal/ex1/controllers/contratos.js
@@ -6,3 +6,26 @@ module.exports.list = () => {
         .find()
         .exec()
 }
+
+//NAO ESTA FUNCIONAL
+module.exports.findById = id => {
+    return Contracts
+        .findOne({_id : id})
+        .exec()
+}
+
+module.exports.findEntidade = function(id) {
+    return Contratos.find({ entidade_comunicante: id });
+}
+
+module.exports.findTipo = function(id) {
+    return Contratos.find({ tipoprocedimento: id });
+}
+
+module.exports.listEntidades = () => {
+    return Contratos.distinct("entidade_comunicante").sort()
+}
+
+module.exports.listTipos = () => {
+    return Contratos.distinct("tipoprocedimento").sort()
+}
--- ENGWEB2024-Normal/ex1/routes/contratos.js
+++ ENGWEB2024-Normal/ex1/routes/contratos.js
@@ -1,16 +1,42 @@
+// routes/contratos.js
+
 var express = require('express');
 var router = express.Router();
 var Contratos = require('../controllers/contratos');
-const contratos = require('../models/contratos');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  Contratos.list()
+  if (req.query.entidade) {
+    Contratos.findEntidade(req.query.entidade)
+      .then(data => res.jsonp(data))
+      .catch(erro => res.jsonp(erro));
+  } else if (req.query.tipo) {
+    Contratos.findTipo(req.query.tipo)
+      .then(data => res.jsonp(data))
+      .catch(erro => res.jsonp(erro));
+  } else {
+    Contratos.list()
+      .then(data => res.jsonp(data))
+      .catch(erro => res.jsonp(erro));
+  }
+});
+
+router.get('/entidades', function(req, res){
+  Contratos.listEntidades()
     .then(data => res.jsonp(data))
-    .catch(erro => {
-      console.log(erro)
-      res.jsonp(erro)
-    })
+    .catch(erro => res.jsonp(erro))
+});
+
+router.get('/tipos', function(req, res){
+  Contratos.listTipos()
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
+});
+
+//ALGUMA COISA ERRADA
+router.get('/:id', function(req, res){
+  Contratos.findById(req.params.id)
+    .then(data => res.jsonp(data))
+    .catch(erro => res.jsonp(erro))
 });
 
 module.exports = router;
